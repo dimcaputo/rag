6bd325f2e2ca3287fd5ca24fefc0c3c1190597d9
Dimitri Caputo <dim.caputo@gmail.com>
2025-11-04 15:31:11 +0100

    made it better

--- query.py
+++ query.py
@@ -6,32 +6,49 @@ from langchain_core.runnables import chain
 from langchain_milvus import Milvus
 import sys
 import os
+import requests
+import ollama
+
+
+req = requests.get("http://localhost:11434/api/tags").json()
+model_is_nomic = [True for model in req['models'] if "nomic-embed-text" in model['name']]
+if not any(model_is_nomic):
+    os.system("ollama pull nomic-embed-text")
+    
+    
+embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+vector_store = Milvus(
+    embedding_function=embeddings,
+    connection_args={"uri": "./milvus_example.db"},
+    index_params={"index_type": "FLAT", "metric_type": "L2"},
+)
+
+@chain
+def retriever(query: str) -> List[Document]:
+    return vector_store.similarity_search_with_score(query, k=10)
+
+def get_results(query: str, retriever_func) -> list:
+    results = {}
+    response = retriever_func.batch(
+        [
+            query
+        ],
+    )
+    for i, doc in enumerate(sorted(response[0], key=lambda x:x[1], reverse=True)):
+        results[i]= {'score':round(doc[1], 2), 'chunk':doc[0].page_content}
+
+    return results
 
 if __name__ == "__main__":
 
     query = sys.argv[1]
 
-    os.system("ollama pull nomic-embed-text")
-    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    results = get_results(query, retriever)
 
-    vector_store = Milvus(
-        embedding_function=embeddings,
-        connection_args={"uri": "./milvus_example.db"},
-        index_params={"index_type": "FLAT", "metric_type": "L2"},
-    )
-
-    @chain
-    def retriever(query: str) -> List[Document]:
-        return vector_store.similarity_search_with_score(query, k=5)
-
-    response = retriever.batch(
-        [
-            query
-        ],
-    )
-
-
-    for doc in sorted(response[0], key=lambda x:x[1], reverse=True):
-        print("\n")
-        print(f"Score = {doc[1]:.2f}")
-        print(doc[0].page_content)
+    for i,item in results:
+        print(f"Score = {item['score']}")
+        print("Chunk " + "*"*50)
+        print(item['chunk'])
+        print('*'*50)
+        print('\n')
