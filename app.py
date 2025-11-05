3179319c45cdddedd2b6319c1e4c6e43497d4f8e
Dimitri Caputo <dim.caputo@gmail.com>
2025-11-05 17:57:57 +0100

    modified to add response

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import chainlit as cl
+from agent import Inquisita
 
+
+inquisita = Inquisita()
+
+@cl.on_chat_start
+def on_chat_start():
+    print("A new chat session has started!")
 
 @cl.on_message
 async def main(message: cl.Message):
     # Your custom logic goes here...
-
+    inquisita.query(message.content)
     # Send a response back to the user
     await cl.Message(
-        content=f"Received: {message.content}",
+        content=f"{inquisita.reply}",
     ).send()
