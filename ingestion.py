90b5cb072c733e0ac39424090b8ad0d773c99db0
Dimitri Caputo <dim.caputo@gmail.com>
2025-11-07 11:05:27 +0100

    change chunk size and overlap

--- ingestion.py
+++ ingestion.py
@@ -51,8 +51,8 @@ if __name__ == "__main__":
 
         print("\n## SPLITTING FILES ####################################\n")
         text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,  # chunk size (characters)
-            chunk_overlap=200,  # chunk overlap (characters)
+            chunk_size=200,  # chunk size (characters)
+            chunk_overlap=5,  # chunk overlap (characters)
             add_start_index=True,  # track index in original document
         )
         all_splits = text_splitter.split_documents(docs)
